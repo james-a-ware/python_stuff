d65b2983624169efdbabdb2178c1b55a2ca1795a
James A. Ware <jamesaware@rocketmail.com>
2021-12-28 17:05:50 +0000

    v2 up not working

---
+++ DUGA-main/num_picker.py
@@ -0,0 +1,20 @@
+
+import random
+
+num_lst1=[1,2,3]
+num_lst2=[1,2,3]
+
+def low-num-gen();
+    for i in num_list1;
+    i = random(int)
+
+
+def low-num-gen();
+    for i in num_list1;
+    i = random(int)
+
+
+#pic 3 random low
+#lic 3 random high
+#check for duplicate
+#check against a db for duplicate list
