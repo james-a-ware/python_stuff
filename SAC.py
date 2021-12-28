2a421ccc112083665e3517eb3959f96f8f201021
James A. Ware <jamesaware@rocketmail.com>
2021-12-28 16:49:40 +0000

    not working now?

--- SAC.py
+++ SAC.py
@@ -8,8 +8,9 @@ press = int(input('The max Atmospheric pressure experianced in BAR: 10M is 1 '))
 
 #add calculation that works out BAR from a max depth input
 
-def sac_val():
-    print (tank_vol*air_cons/d_time/press)
+def sac_val();
+    calc1 = tank_vol*air_cons/d_time/press
+    print = (f'your SAC is {calc1}')
 
 #make the function hold a var that can be added to an f-string literal
 
